4bde0b1e3fc57733d52d000e250be5903d3ef9a7
Krishna Prasad Kuchur <krishnakuchur4@gmail.com>
2017-11-29 16:38:18 -0700

    Enhanced Video Playback, provided ui for username and logout feature.

--- app/src/main/java/group14/mc/asu/edu/brainnetteam14/SplashScreen.java
+++ app/src/main/java/group14/mc/asu/edu/brainnetteam14/SplashScreen.java
@@ -2,8 +2,10 @@ package group14.mc.asu.edu.brainnetteam14;
 
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -13,6 +15,7 @@ import com.bumptech.glide.request.RequestOptions;
 public class SplashScreen extends AppCompatActivity {
 
     private static final String imageUrl = "https://intimacyheals.files.wordpress.com/2017/03/energyflow.gif?w=788";
+    private String TAG = "SplashScreen";
     //private static final String imageUrl  = "https://billybeyondblog.files.wordpress.com/2017/02/tumblr_okedamfony1vfo0eko1_1280.gif?w=584&h=584";
     ImageView gifImageView;
     @Override
@@ -20,6 +23,7 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         gifImageView = findViewById(R.id.imageView);
+        calculateNetWorkDelay();
         Glide.with(this)
                 .load(imageUrl)
                 .apply(new RequestOptions()
@@ -39,5 +43,16 @@ public class SplashScreen extends AppCompatActivity {
 
             }
         }.start();
+    }
+
+    public void calculateNetWorkDelay(){
+
+        OffloadingParams cloudParams = new OffloadingParams(getApplicationContext(), "Cloud");
+        OffloadingParams fogParams = new OffloadingParams(getApplicationContext(), "Fog");
+        
+//        OffloadingParams offloadingParams = new OffloadingParams(getApplication(), "Cloud");
+//        String d = OffloadingParams.executeCmd("ping -c 100 -w 1 8.8.8.8", false);
+//        String[] dist = d.split("/");
+//        Log.d(TAG,"AVG:"+dist[4]);
     }
 }
